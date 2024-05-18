e12cd8414aafa2c9a7bffa5e2db634edbaf6a099
vishwajitvm <vishwajitmall50@gmail.com>
2024-05-19 00:33:26 +0530

    changes for firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { firebaseConfig } from '../../firebaseConfig';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +12,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'HandyWeb-Utilities';
+
+  constructor() {
+    initializeApp(firebaseConfig);
+  }
+  
 }
